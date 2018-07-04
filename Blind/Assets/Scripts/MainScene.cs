aa959bd6d0ece4dda2dd78a249dcaa564ed42dd3
Ivan <ivandenisik@gmail.com>
2018-07-04 21:35:36 +0300

    Полностью собрал задуманные функции.

--- Blind/Assets/Scripts/MainScene.cs
+++ Blind/Assets/Scripts/MainScene.cs
@@ -6,7 +6,26 @@ using UnityEngine.SceneManagement;
 
 public class MainScene : MonoBehaviour {
 	
+	IEnumerator WaitAndEndScene(){
+		yield return new WaitForSeconds(2f);
+		EndScene();
+	}
+	
+	public void PointerEnter(){
+		StartCoroutine(WaitAndEndScene());
+	}
+	
+	public void PointerExit(){
+		StopAllCoroutines();
+	}
+	
 	public void EndScene(){
-		Application.Quit();
+		#if UNITY_EDITOR
+		// Application.Quit() does not work in the editor so
+		// UnityEditor.EditorApplication.isPlaying need to be set to false to end the game
+		UnityEditor.EditorApplication.isPlaying = false;
+        #else
+		   Application.Quit();
+        #endif
 	}
 }
